3019104263df582dca07442d2d5403373fd7be35
lacfo <liyuanlacfo1990@gmail.com>
2017-01-16 20:39:32 +0800

    Install gem devise and validate the job columns

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,4 @@
 class Job < ApplicationRecord
+	validates :title, :description, :wage_upper_bound, :wage_lower_bound, :field, :location, presence: true
+	validates :wage_lower_bound, :wage_upper_bound, numericality: {:greater_than => 0}
 end
